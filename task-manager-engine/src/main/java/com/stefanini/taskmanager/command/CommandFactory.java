e1acb54a37d9664828af1a32ff1490632fdb6056
SandraRusu17 <sandra.rusu17@gmail.com>
2021-08-26 12:52:56 +0300

    -refactor some code

--- task-manager-engine/src/main/java/com/stefanini/taskmanager/command/CommandFactory.java
+++ task-manager-engine/src/main/java/com/stefanini/taskmanager/command/CommandFactory.java
@@ -1,11 +1,9 @@
 package com.stefanini.taskmanager.command;
 
-import com.stefanini.taskmanager.command.*;
 import com.stefanini.taskmanager.command.exceptions.InvalidCommandException;
-import com.stefanini.taskmanager.entity.User;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.stefanini.taskmanager.command.utils.CommandParameterParser.*;
+import static com.stefanini.taskmanager.utils.CommandParameterParser.*;
 
 @Slf4j
 public class CommandFactory {
@@ -25,46 +23,46 @@ public class CommandFactory {
 
 
     public static Command parseCommandArguments(String[] arguments) throws InvalidCommandException {
-        String joinedArguments = String.join(" ", arguments); //so we can have parameter values with spaces
-        String[] commandAndParameters = joinedArguments.split(" -"); //split by ' -' again so we have the command and its parameters
-        final String command = commandAndParameters[COMMAND];
+//        String joinedArguments = String.join(" ", arguments); //so we can have parameter values with spaces
+//        String[] commandAndParameters = joinedArguments.split(" -"); //split by ' -' again so we have the command and its parameters
+//        final String command = arguments[COMMAND];
 
-        switch (command) {
+        switch (arguments[0]) {
             case CREATE_USER_COMMAND:
-                if (commandAndParameters.length < 4) {
+                if (arguments.length < 4) {
                     throw new InvalidCommandException("Oops. Please refer to the usage of the command : " + "-createUser -fn='FirstName' -ln='LastName' -un='UserName'");
                 }
-                return new AddUserCommand(getUsername(commandAndParameters),
-                        getFirstName(commandAndParameters),
-                        getLastName(commandAndParameters));
+                return new AddUserCommand(getUsername(arguments),
+                        getFirstName(arguments),
+                        getLastName(arguments));
 
             case SHOW_ALL_USERS_COMMAND:
                 return new GetAllUsersCommand();
 
             case ADD_TASK_FOR_COMMAND:
-                if (commandAndParameters.length < 4) {
+                if (arguments.length < 4) {
                     throw new InvalidCommandException(
                             "Oops. Please refer to the usage of the command : -addTask -un='UserName' -tt='TaskTitle' -td='TaskDescription'");
                 }
-                return new AddTaskCommand(getTaskTitle(commandAndParameters),
-                        getTaskDescription(commandAndParameters),
-                        getUsername(commandAndParameters));
+                return new AddTaskCommand(getTaskTitle(arguments),
+                        getTaskDescription(arguments),
+                        getUsername(arguments));
 
             case DELETE_TASK_BY_TITLE_FOR_COMMAND:
-                if (commandAndParameters.length < 3) {
+                if (arguments.length < 3) {
                     throw new InvalidCommandException(
                             "Oops. Please refer to the usage of the command: -deleteTask -un='UserName' -tt='TaskTitle'");
                 }
-                return new DeleteTaskCommand(getTaskTitle(commandAndParameters), getUsername(commandAndParameters));
+                return new DeleteTaskCommand(getTaskTitle(arguments), getUsername(arguments));
 
             case SHOW_TASKS_FOR_COMMAND:
-                if (commandAndParameters.length < 2) {
+                if (arguments.length < 2) {
                     throw new InvalidCommandException(
                             "Oops. Please refer to the usage of the command : -showTasks -un='UserName'");
                 }
-                return new GetTasksCommand(getUsername(commandAndParameters));
+                return new GetTasksCommand(getUsername(arguments));
             default:
-                throw new InvalidCommandException("Oops. Unknown command [" + command + "] Please use one of the following commands: -createUser -showAllUsers -addTask");
+                throw new InvalidCommandException("Oops. Unknown command [" + arguments[0] + "] Please use one of the following commands: -createUser -showAllUsers -addTask");
         }
     }
 }
