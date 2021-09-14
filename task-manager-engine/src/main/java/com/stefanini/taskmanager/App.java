62a9ac82a61281c6071e4134470776f6e35027a9
SandraRusu17 <sandra.rusu17@gmail.com>
2021-09-14 17:54:53 +0300

    -uncomment code

--- task-manager-engine/src/main/java/com/stefanini/taskmanager/App.java
+++ task-manager-engine/src/main/java/com/stefanini/taskmanager/App.java
@@ -4,19 +4,7 @@ package com.stefanini.taskmanager;
 import com.stefanini.taskmanager.command.Command;
 import com.stefanini.taskmanager.command.CommandFactory;
 import com.stefanini.taskmanager.command.exceptions.InvalidCommandException;
-import com.stefanini.taskmanager.entity.Email;
-import com.stefanini.taskmanager.entity.Task;
-import com.stefanini.taskmanager.entity.User;
-import com.stefanini.taskmanager.service.EmailService;
-import com.stefanini.taskmanager.service.ServiceFactory;
-import com.stefanini.taskmanager.service.UserService;
 import com.stefanini.taskmanager.service.exceptions.UserNotFoundException;
-import com.stefanini.taskmanager.service.impl.EmailServiceImpl;
-import com.stefanini.taskmanager.service.impl.UserServiceImpl;
-import org.hibernate.SessionFactory;
-
-import javax.mail.Session;
-import javax.transaction.Transaction;
 import java.io.IOException;
 
 
@@ -24,13 +12,13 @@ public class App {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws InvalidCommandException, IOException {
-//        Command command = CommandFactory.parseCommandArguments(args);
-//        try {
-//            command.execute();
-//        } catch (InvalidCommandException | UserNotFoundException e) {
-//            log.error("Something bad happened during entering command",e.getMessage());
-//            System.exit(0);
-//        }
+        Command command = CommandFactory.parseCommandArguments(args);
+        try {
+            command.execute();
+        } catch (InvalidCommandException | UserNotFoundException e) {
+            log.error("Something bad happened during entering command",e.getMessage());
+            System.exit(0);
+        }
 
     }
 }
